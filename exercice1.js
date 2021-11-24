c1c737d7b4c1954354fa073f929ab5b61e094333
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-24 16:28:57 +0100

    Exo 78

--- exercice1.js
+++ exercice1.js
@@ -1,12 +1,22 @@
-let monTexte = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+let monText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
 
 // Completer le code suivant pour afficher la longueur de la variable monTexte
-
-alert(monTexte.);
+alert(monText.length);
 
 
 //Ajouter du code ci dessous pour réaliser la suite de l'exercice
+let positionLettreI = monText.indexOf('i');
+let position = document.getElementById('position');
+position.innerHTML = positionLettreI;
 
+let maj = monText.toUpperCase();
+let stockMonText = document.getElementById('chaineMaj');
+stockMonText.innerHTML = maj;
+
+for ( let i = 0; i < monText.length; i++){
+    let positionLettreA = monText.indexOf('a');
+    alert('La lettre "A" trouver');
+}
 
 
 
